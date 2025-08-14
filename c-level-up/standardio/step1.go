4765efbb28de9dcededf7297b597b0cf1d6ef60e
sudamichiyo <mitchmitch87@ezweb.ne.jp>
2025-08-15 00:06:11 +0900

    [add] c-level-up/standardio/step1.go

---
+++ c-level-up/standardio/step1.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// 文字列sを標準入力から入力
+	var s string
+	fmt.Scanf("%s", &s)
+
+	// 文字列をそのまま出力
+	fmt.Println(s)
+}
+
+// 問題URL:https://paiza.jp/works/mondai/c_rank_level_up_problems/c_rank_std_in_out_step1
