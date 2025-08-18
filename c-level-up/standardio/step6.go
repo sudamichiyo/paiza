2ea277954e45c8f5250fd85de47b9ae97f36cc1a
sudamichiyo <mitchmitch87@ezweb.ne.jp>
2025-08-18 17:51:08 +0900

    [add] c-level-up/standardio/step6.go

---
+++ c-level-up/standardio/step6.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 整数nを標準入力から入力
+	var n int
+	fmt.Scanf("%d", &n)
+
+	// n個の整数を半角スペース区切りで標準入力から入力
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		fmt.Scanf("%d", &a[i])
+	}
+
+	// 整数を改行区切りで出力
+	for _, v := range a {
+		fmt.Println(v)
+	}
+}
+
+// 問題URL:https://paiza.jp/works/mondai/c_rank_level_up_problems/c_rank_std_in_out_step6
