e0239973946750f45151b8ef88bfe06adbaf4b32
sudamichiyo <mitchmitch87@ezweb.ne.jp>
2025-11-12 16:03:28 +0900

    [add] c-skillcheck-archive/word-count-04.go

---
+++ c-skillcheck-archive/word-count-04.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	// 半角スペースで区切られた長さNの文字列を標準入力から入力
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+
+	// 文字列を半角スペース区切りでスライスに格納
+	inputs := strings.Split(scanner.Text(), " ")
+
+	// スライス内の単語が新出であればその単語を単語リストに格納
+	// 単語リストを格納するスライスを定義
+	wordlist := []string{}
+
+	// 単語の存在を確認し、新出単語の場合は単語リストに単語を追加する
+	for _, word := range inputs {
+		if !contains04(wordlist, word) { //新出単語の場合
+			wordlist = append(wordlist, word)
+		}
+	}
+
+	// 単語リストを表示
+	for _, w := range wordlist {
+		fmt.Println(w)
+	}
+
+}
+
+func contains04(slice []string, word string) bool {
+	for _, v := range slice {
+		if v == word {
+			return true
+		}
+	}
+	return false
+}
+
+// 問題URL:https://paiza.jp/works/mondai/c_rank_skillcheck_archive/word-count_04
