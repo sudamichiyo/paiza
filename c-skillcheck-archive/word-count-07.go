ed30dc7140799304db58a88ca823bfe6ae94af0b
sudamichiyo <mitchmitch87@ezweb.ne.jp>
2025-11-16 01:03:00 +0900

    [add] c-skillcheck-archive/word-count-07.go

---
+++ c-skillcheck-archive/word-count-07.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	// 半角スペースで区切られた長さNの文字列を標準入力から入力
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+
+	// 文字列を半角スペース区切りでスライスに格納
+	inputs := strings.Split(scanner.Text(), " ")
+
+	// スライス内の単語が新出であればその単語を単語リストに格納
+	// 単語リストを格納するスライスを定義
+	wordlist := []string{}
+
+	//単語をキー、単語の出現回数をバリューとするマップを作成
+	count := make(map[string]int, len(inputs))
+
+	// 単語の存在を確認し、新出単語の場合は単語リストに単語を追加する
+	for _, word := range inputs {
+		if !contains07(wordlist, word) { //新出単語の場合
+			wordlist = append(wordlist, word)
+		}
+		// 単語の出現回数をカウント
+		count[word]++
+	}
+
+	// 単語リストを表示
+	for _, w := range wordlist {
+		fmt.Println(w)
+	}
+
+	// ぞれぞれの単語の出現回数を出力
+	for _, w := range wordlist {
+		fmt.Println(count[w])
+	}
+
+}
+
+func contains07(slice []string, word string) bool {
+	for _, v := range slice {
+		if v == word {
+			return true
+		}
+	}
+	return false
+}
+
+// 問題URL:https://paiza.jp/works/mondai/c_rank_skillcheck_archive/word-count_07
