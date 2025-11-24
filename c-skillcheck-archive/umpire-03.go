ac959b4c104ada9f5af2ac7cc8ee18845ab3e348
sudamichiyo <mitchmitch87@ezweb.ne.jp>
2025-11-24 11:41:38 +0900

    [add] c-skillcheck-archive/umpire-03.go

---
+++ c-skillcheck-archive/umpire-03.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 投球数を表す整数nを標準入力から入力
+	var n int
+	fmt.Scanf("%d", &n)
+
+	// n回投球結果(strike または ball)を標準入力から入力
+	pitching := make([]string, 0, n)
+	for i := 0; i < n; i++ {
+		var p string
+		fmt.Scanf("%s", &p)
+		pitching = append(pitching, p)
+	}
+
+	// 投球結果を出力
+	for _, pitch := range pitching {
+		fmt.Println(pitch)
+	}
+}
+
+// 問題URL:https://paiza.jp/works/mondai/c_rank_skillcheck_archive/umpire_04
