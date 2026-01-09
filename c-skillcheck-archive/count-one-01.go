895bf43bec5821a3223c477ec5979b8fc99508f7
sudamichiyo <mitchmitch87@ezweb.ne.jp>
2026-01-10 02:34:49 +0900

    [add] c-skillcheck-archive/count-one-01.go

---
+++ c-skillcheck-archive/count-one-01.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 10進数の整数nを標準入力から入力
+	var n int
+	fmt.Scanf("%d", &n)
+
+	// nを出力
+	fmt.Println(n)
+}
+
+// 問題URL:https://paiza.jp/works/mondai/c_rank_skillcheck_archive/count_one_01
